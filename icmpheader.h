6674f8429b76bc7926906d22259056ba941f388e
Thomas Dreibholz <dreibh@simula.no>
2015-09-22 14:56:56 +0200

    Started to write Traceroute for IPv6.

---
+++ icmpheader.h
@@ -0,0 +1,115 @@
+// NorNet Trace Service
+// Copyright (C) 2015 by Thomas Dreibholz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Contact: dreibh@simula.no
+
+#ifndef ICMPHEADER_H
+#define ICMPHEADER_H
+
+#include <algorithm>
+
+
+// ==========================================================================
+// From RFC 4443:
+//
+//        0                   1                   2                   3
+//        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//       |     Type      |     Code      |          Checksum             |
+//       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//       |           Identifier          |        Sequence Number        |
+//       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//       |     Data ...
+//       +-+-+-+-+-
+//
+// ==========================================================================
+
+class ICMPHeader
+{
+public:
+  enum {
+     IPv4EchoRequest  = 8,
+     IPv4EchoReply    = 0,
+     IPv4TimeExceeded = 11,
+     IPv4Unreachable  = 3,
+
+     IPv6EchoRequest  = 129,
+     IPv6EchoReply    = 128,
+     IPv6TimeExceeded = 3,
+     IPv6Unreachable  = 1
+  };
+
+  ICMPHeader() {
+     std::fill(data, data + sizeof(data), 0);
+  }
+
+  inline unsigned char type()        const { return data[0];      }
+  inline unsigned char code()        const { return data[1];      }
+  inline unsigned short checksum()   const { return decode(2, 3); }
+  inline unsigned short identifier() const { return decode(4, 5); }
+  inline unsigned short seqNumber()  const { return decode(6, 7); }
+
+  inline void type(unsigned char type)          { data[0] = type;         }
+  inline void code(unsigned char code)          { data[1] = code;         }
+  inline void checksum(unsigned short checksum) { encode(2, 3, checksum); }
+  inline void identifier(unsigned short id)     { encode(4, 5, id);       }
+  inline void seqNumber(unsigned short seqNum)  { encode(6, 7, seqNum);   }
+
+   inline friend std::istream& operator>>(std::istream& is, ICMPHeader& header) {
+      return is.read(reinterpret_cast<char*>(header.data), 8);
+   }
+
+   inline friend std::ostream& operator<<(std::ostream& os, const ICMPHeader& header) {
+      return os.write(reinterpret_cast<const char*>(header.data), 8);
+   }
+
+   private:
+   inline unsigned short decode(int a, int b) const {
+      return((data[a] << 8) + data[b]);
+   }
+
+   inline void encode(int a, int b, unsigned short n) {
+      data[a] = static_cast<unsigned char>(n >> 8);
+      data[b] = static_cast<unsigned char>(n & 0xFF);
+   }
+
+   unsigned char data[8];
+};
+
+
+// Internet-16 checksum according to RFC 1071
+template <typename Iterator> void computeInternet16(ICMPHeader& header, Iterator bodyBegin, Iterator bodyEnd)
+{
+   uint32_t sum =
+      (header.type() << 8) +
+      header.code() +
+      header.identifier() +
+      header.seqNumber();
+
+   Iterator body_iter = bodyBegin;
+   while (body_iter != bodyEnd) {
+      sum += (static_cast<unsigned char>(*body_iter++) << 8);
+      if (body_iter != bodyEnd) {
+         sum += static_cast<unsigned char>(*body_iter++);
+      }
+   }
+
+   sum = (sum >> 16) + (sum & 0xFFFF);
+   sum += (sum >> 16);
+   header.checksum(static_cast<unsigned short>(~sum));
+}
+
+#endif
--- traceservice.cc
+++ traceservice.cc
@@ -24,7 +24,10 @@
 
 #include <set>
 #include <iostream>
+#include <boost/thread.hpp>
 #include <boost/asio.hpp>
+
+#include "icmpheader.h"
 
 
 std::set<boost::asio::ip::address> sourceArray;
@@ -44,6 +47,121 @@ void addAddress(std::set<boost::asio::ip::address>& array,
 }
 
 
+class Traceroute : public boost::noncopyable
+{
+   public:
+   Traceroute(const boost::asio::ip::address& source,
+              const boost::asio::ip::address& destination,
+              const unsigned int              packetsPerHop = 3,
+              const unsigned int              maxTTL        = 35);
+   ~Traceroute();
+   void run();
+
+   inline bool isIPv6() const {
+      return(SourceAddress.is_v6());
+   }
+
+   private:
+   const unsigned int              PacketsPerHop;
+   const unsigned int              MaxTTL;
+   boost::asio::io_service         IO;
+   boost::asio::ip::address        SourceAddress;
+   boost::asio::ip::icmp::endpoint SourceEndpoint;
+   boost::asio::ip::address        DestinationAddress;
+   boost::asio::ip::icmp::endpoint DestinationEndpoint;
+   boost::asio::ip::icmp::socket   ICMPSocket;
+};
+
+
+Traceroute::Traceroute(const boost::asio::ip::address& source,
+                       const boost::asio::ip::address& destination,
+                       const unsigned int              packetsPerHop,
+                       const unsigned int              maxTTL)
+   : PacketsPerHop(packetsPerHop),
+     MaxTTL(maxTTL),
+     IO(),
+     SourceAddress(source),
+     SourceEndpoint(SourceAddress, 0),
+     DestinationAddress(destination),
+     DestinationEndpoint(DestinationAddress, 0),
+     ICMPSocket(IO, (isIPv6() == true) ? boost::asio::ip::icmp::v6() : boost::asio::ip::icmp::v4())
+{
+}
+
+
+Traceroute::~Traceroute()
+{
+}
+
+
+void Traceroute::run()
+{
+   unsigned int   identifier = ::getpid();
+   unsigned short seqNumber  = 0;
+
+   ICMPSocket.non_blocking(true);
+
+   puts("s1");
+   for(unsigned int ttl = 1; ttl <= MaxTTL; ttl++) {
+      const boost::asio::ip::unicast::hops option(ttl);
+      ICMPSocket.set_option(option);
+
+      // Create an ICMP header for an echo request.
+      ICMPHeader echoRequest;
+      echoRequest.type((isIPv6() == true) ? ICMPHeader::IPv6EchoRequest : ICMPHeader::IPv4EchoRequest);
+      echoRequest.code(0);
+      echoRequest.identifier(identifier);
+      echoRequest.seqNumber(++seqNumber);
+      const std::string body("");
+      computeInternet16(echoRequest, body.begin(), body.end());
+
+      // Encode the request packet.
+      boost::asio::streambuf request_buffer;
+      std::ostream os(&request_buffer);
+      os << echoRequest << body;
+
+      // Send the request.
+      size_t s = ICMPSocket.send_to(request_buffer.data(), DestinationEndpoint);
+//       printf("s=%d\n",(int)s);
+   }
+
+   ICMPSocket.non_blocking(false);
+
+   for(unsigned int ttl = 1; ttl <= MaxTTL; ttl++) {
+
+             // Recieve some data and parse it.
+            std::vector<boost::uint8_t> data(1500,0);
+            const std::size_t nr(
+               ICMPSocket.receive(
+                  boost::asio::buffer(data) ) );
+            printf("nr=%d\n",(int)nr);
+            if( nr < 16 )
+            {
+               throw std::runtime_error("To few bytes returned.");
+            }
+            boost::asio::ip::address address;
+            if(isIPv6()) {
+               boost::asio::ip::address_v6::bytes_type v6address;
+               for(size_t i = 0;i < 16; i++) {
+                  v6address[i] = data[16 + i];
+               }
+               address = boost::asio::ip::address_v6(v6address, 0);
+            }
+            else {
+               boost::asio::ip::address_v4::bytes_type v4address;
+               for(size_t i = 0;i < 4; i++) {
+                  v4address[i] = data[12 + i];
+               }
+               address = boost::asio::ip::address_v4(v4address);
+            }
+            std::cout << address.to_string() << '\n';
+            if( address == DestinationAddress )
+            {
+               break;
+            }
+   }
+}
+
 
 int main(int argc, char** argv)
 {
@@ -56,6 +174,18 @@ int main(int argc, char** argv)
       }
       else {
          std::cerr << "Usage: " << argv[0] << " -source=source ... -destination=destination ..." << std::endl;
+      }
+   }
+   if( (sourceArray.size() < 1) ||
+       (destinationArray.size() < 1) ) {
+      std::cerr << "ERROR: At least one source and destination are needed!" << std::endl;
+      ::exit(1);
+   }
+
+   for(std::set<boost::asio::ip::address>::iterator sourceIterator = sourceArray.begin(); sourceIterator != sourceArray.end(); sourceIterator++) {
+      for(std::set<boost::asio::ip::address>::iterator destinationIterator = destinationArray.begin(); destinationIterator != destinationArray.end(); destinationIterator++) {
+         Traceroute t(*sourceIterator, *destinationIterator);
+         t.run();
       }
    }
 }
