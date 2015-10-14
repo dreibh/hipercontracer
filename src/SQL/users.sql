c788e8e879ff5170f0e59707b847db7d78034014
Thomas Dreibholz <dreibh@simula.no>
2015-10-14 11:17:06 +0200

    Configuration update.

--- src/SQL/users.sql
+++ src/SQL/users.sql
@@ -1,5 +1,42 @@
 -- STEP 3: Create Users
 -- sudo -u postgres psql pingtraceroutedb <users.sql
+--
+-- =================================================================
+--          #     #                 #     #
+--          ##    #   ####   #####  ##    #  ######   #####
+--          # #   #  #    #  #    # # #   #  #          #
+--          #  #  #  #    #  #    # #  #  #  #####      #
+--          #   # #  #    #  #####  #   # #  #          #
+--          #    ##  #    #  #   #  #    ##  #          #
+--          #     #   ####   #    # #     #  ######     #
+--
+--       ---   The NorNet Testbed for Multi-Homed Systems  ---
+--                       https://www.nntb.no
+-- =================================================================
+--
+-- High-Performance Connectivity Tracer (HiPerConTracer)
+-- Copyright (C) 2015 by Thomas Dreibholz
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Contact: dreibh@simula.no
+
+
+-- ##########################################################
+-- !!! IMPORTANT: Change the passwords before deployment! !!!
+-- ##########################################################
+
 
 REVOKE ALL ON DATABASE pingtraceroutedb FROM importer;
 REVOKE ALL ON Ping FROM importer;
