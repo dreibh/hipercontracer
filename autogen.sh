57cf25fed074e28e1df4d62df53a34a969dd0e59
Thomas Dreibholz <dreibh@simula.no>
2017-11-18 11:53:17 +0100

    Changed build system to CMake.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-./bootstrap && \
-./configure $@ && \
-( gmake || make )
+rm -f CMakeCache.txt
+cmake -DCMAKE_INSTALL_PREFIX=/usr $@ .
+
+# ------ Obtain number of cores ---------------------------------------------
+# Try Linux
+cores=`getconf _NPROCESSORS_ONLN 2>/dev/null`
+if [ "$cores" == "" ] ; then
+   # Try FreeBSD
+   cores=`sysctl -a | grep 'hw.ncpu' | cut -d ':' -f2 | tr -d ' '`
+fi
+if [ "$cores" == "" ] ; then
+   cores="1"
+fi
+
+gmake -j$cores || make -j$cores
