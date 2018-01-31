7fe80e5b6bd17110eb7a11fa58ad41110889159f
Thomas Dreibholz <dreibh@simula.no>
2018-01-31 18:14:43 +0100

    Added GNU R MongoDB test script.

---
+++ src/t3.R
@@ -0,0 +1,27 @@
+# install.packages("mongolite")
+# install.packages("data.table")
+
+library(mongolite)
+library(data.table)
+
+
+dbserver   <- "10.1.1.47" # rolfsbukta.alpha.test"
+dbport     <- 27017
+dbuser     <- "researcher"
+dbpassword <- "!researcher!"
+database   <- "pingtraceroutedb"
+
+
+# See https://jeroen.github.io/mongolite/connecting-to-mongodb.html#authentication
+URL     <- sprintf("mongodb://%s:%s@%s:%d/%s?ssl=true",
+                   dbuser, dbpassword, dbserver, dbport, database)
+options <- ssl_options(weak_cert_validation=TRUE, allow_invalid_hostname=TRUE)
+print(URI)
+
+
+ping <- mongo("ping", url = URL, options = options)
+traceroute <- mongo("traceroute", url = URL, options = options)
+# print(ping)
+
+d <- data.table(traceroute$find('{}'))
+print(d)
