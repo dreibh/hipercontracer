8fe1036dd5fe2c8d9fd51418b832da98f5478fe5
Thomas Dreibholz <dreibh@simula.no>
2018-01-31 18:23:21 +0100

    Updated test.

--- src/t3.R
+++ src/t3.R
@@ -16,12 +16,12 @@ database   <- "pingtraceroutedb"
 URL     <- sprintf("mongodb://%s:%s@%s:%d/%s?ssl=true",
                    dbuser, dbpassword, dbserver, dbport, database)
 options <- ssl_options(weak_cert_validation=TRUE, allow_invalid_hostname=TRUE)
-print(URI)
+print(URL)
 
 
 ping <- mongo("ping", url = URL, options = options)
 traceroute <- mongo("traceroute", url = URL, options = options)
 # print(ping)
 
-d <- data.table(traceroute$find('{}'))
+d <- data.table(traceroute$find('{ "timestamp" : 16777216 }'))
 print(d)
