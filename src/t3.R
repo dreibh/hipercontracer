a487429727ec43084e97db244b4cbe8d8a46af6b
Thomas Dreibholz <dreibh@simula.no>
2018-02-01 20:48:21 +0100

    Updated test script.

--- src/t3.R
+++ src/t3.R
@@ -1,11 +1,12 @@
 # install.packages("mongolite")
 # install.packages("data.table")
+# install.packages("iptools")
 
 library(mongolite)
 library(data.table)
 
 
-dbserver   <- "10.1.1.47" # rolfsbukta.alpha.test"
+dbserver   <- "10.1.1.47"   # rolfsbukta.alpha.test"
 dbport     <- 27017
 dbuser     <- "researcher"
 dbpassword <- "!researcher!"
@@ -23,5 +24,25 @@ ping <- mongo("ping", url = URL, options = options)
 traceroute <- mongo("traceroute", url = URL, options = options)
 # print(ping)
 
-d <- data.table(traceroute$find('{ "timestamp" : 16777216 }'))
-print(d)
+
+
+binary_ip_to_string <- function(binaryIP)
+{
+   if(length(binaryIP) == 4) {
+      return("0.0.0.0")
+   }
+   else if(length(binaryIP) == 16) {
+      return("::")
+   }
+   stop("Not an IPv4 or IPv6 address!")
+}
+
+
+tracerouteData <- data.table(traceroute$find('{ "timestamp" : 16777216 }'))
+print(tracerouteData)
+
+for(i in 1:length(tracerouteData$timestamp)) {
+   print(paste0("==== ", as.character(i), " ===="))
+   tracerouteResult <- tracerouteData[i]
+   print(tracerouteResult)
+}
