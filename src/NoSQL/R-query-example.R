92f19f339d8e8ee2f2255e0c032af3d99b868355
Thomas Dreibholz <dreibh@simula.no>
2018-02-05 16:39:33 +0100

    Moved example.

--- src/NoSQL/R-query-example.R
+++ src/NoSQL/R-query-example.R
@@ -1,8 +1,41 @@
+# =================================================================
+#          #     #                 #     #
+#          ##    #   ####   #####  ##    #  ######   #####
+#          # #   #  #    #  #    # # #   #  #          #
+#          #  #  #  #    #  #    # #  #  #  #####      #
+#          #   # #  #    #  #####  #   # #  #          #
+#          #    ##  #    #  #   #  #    ##  #          #
+#          #     #   ####   #    # #     #  ######     #
+#
+#       ---   The NorNet Testbed for Multi-Homed Systems  ---
+#                       https:#www.nntb.no
+# =================================================================
+#
+# High-Performance Connectivity Tracer (HiPerConTracer)
+# Copyright (C) 2015-2018 by Thomas Dreibholz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http:#www.gnu.org/licenses/>.
+#
+# Contact: dreibh@simula.no
+
+
 # install.packages("mongolite")
 # install.packages("data.table")
 # install.packages("iptools")
 # install.packages("Rcpp")   # Needed by "anytime"; pre-installed version is too old!
 # install.packages("anytime")
+
 
 library(mongolite)
 library(data.table)
@@ -17,7 +50,7 @@ dbpassword <- "!researcher!"
 database   <- "pingtraceroutedb"
 
 
-# See https://jeroen.github.io/mongolite/connecting-to-mongodb.html#authentication
+# See https:#jeroen.github.io/mongolite/connecting-to-mongodb.html#authentication
 URL     <- sprintf("mongodb://%s:%s@%s:%d/%s?ssl=true",
                    dbuser, dbpassword, dbserver, dbport, database)
 options <- ssl_options(weak_cert_validation=TRUE, allow_invalid_hostname=TRUE)
