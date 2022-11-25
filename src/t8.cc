689cda1e508dc2c265bc1aefc9db83391c6d31bf
Thomas Dreibholz <dreibh@simula.no>
2022-11-25 17:08:21 +0100

    Some testing.

---
+++ src/t8.cc
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ifaddrs.h>
+#include <netdb.h>
+#include <net/if.h>
+
+int main(int argc, char *argv[])
+{
+    struct ifaddrs *ifaddr;
+    int family, s;
+    char host[NI_MAXHOST];
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+
+    for (struct ifaddrs *ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL)
+            continue;
+        family = ifa->ifa_addr->sa_family;
+
+        if (family == AF_INET || family == AF_INET6) {
+
+            printf("%-8s index=%u %s (%d)\n",
+                   ifa->ifa_name,
+                   if_nametoindex(ifa->ifa_name),
+                   (family == AF_INET) ? "AF_INET" :
+                   (family == AF_INET6) ? "AF_INET6" : "???",
+                   family);
+
+            s = getnameinfo(ifa->ifa_addr,
+                    (family == AF_INET) ? sizeof(struct sockaddr_in) :
+                                          sizeof(struct sockaddr_in6),
+                    host, NI_MAXHOST,
+                    NULL, 0, NI_NUMERICHOST);
+            if (s != 0) {
+                printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+            }
+
+            printf("\t\taddress: <%s>\n", host);
+        }
+    }
+
+    freeifaddrs(ifaddr);
+    exit(EXIT_SUCCESS);
+}
