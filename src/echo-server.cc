9f5a6fb14351ae2cb9126e5225de16a61620852a
Thomas Dreibholz <dreibh@simula.no>
2022-12-11 13:20:48 +0100

    Some improvements.

--- src/echo-server.cc
+++ src/echo-server.cc
@@ -34,27 +34,45 @@
 #include <boost/program_options.hpp>
 
 #include "logger.h"
+#include "tools.h"
 
 
 static boost::asio::ip::udp::endpoint RemoteEndpoint;
 static char                           Buffer[65536];
 
 
+// ###### Handle incoming echo request ######################################
 void handleMessage(boost::asio::ip::udp::socket*    udpSocket,
                    const boost::system::error_code& errorCode,
                    std::size_t                      bytesReceived)
 {
    if(errorCode != boost::asio::error::operation_aborted) {
-      // For security reasons, only respond if remote port != 7.
-      // Otherwise, an attacker could inject a message to cause
-      // an echo loop between two echo servers!
-      if(RemoteEndpoint.port() != 7) {
-         udpSocket->send_to(boost::asio::buffer(Buffer, bytesReceived),
-                            RemoteEndpoint);
+      // ====== Send response ===============================================
+      if(bytesReceived > 0) {
+         // For security reasons, only respond if remote port != 7.
+         // Otherwise, an attacker could inject a message to cause
+         // an echo loop between two echo servers!
+         if(RemoteEndpoint.port() != 7) {
+            boost::system::error_code sendErrorCode;
+            udpSocket->send_to(boost::asio::buffer(Buffer, bytesReceived),
+                              RemoteEndpoint, 0, sendErrorCode);
+            if(sendErrorCode != boost::system::errc::success) {
+               HPCT_LOG(error) << "Error sending " << bytesReceived << " bytes to "
+                               << RemoteEndpoint << ": " << sendErrorCode.message();
+            }
+#if 0
+            else {
+               HPCT_LOG(trace) << "Replied " << bytesReceived << " bytes to "
+                               << RemoteEndpoint;
+            }
+#endif
+         }
+         else {
+            HPCT_LOG(warning) << "Ignoring request from " << RemoteEndpoint;
+         }
       }
-      else {
-         HPCT_LOG(warning) << "Ignoring request from " << RemoteEndpoint;
-      }
+
+      // ====== Wait for next message =======================================
       udpSocket->async_receive_from(boost::asio::buffer(Buffer, sizeof(Buffer)),
                                     RemoteEndpoint,
                                     std::bind(&handleMessage,
@@ -70,6 +88,7 @@ int main(int argc, char *argv[])
 {
    // ====== Initialize =====================================================
    unsigned int             logLevel;
+   std::string              user((getlogin() != nullptr) ? getlogin() : "");
    std::string              localAddressString;
    boost::asio::ip::address localAddress;
    uint16_t                 localPort;
@@ -88,6 +107,9 @@ int main(int argc, char *argv[])
       ( "quiet,q",
            boost::program_options::value<unsigned int>(&logLevel)->implicit_value(boost::log::trivial::severity_level::warning),
            "Quiet logging level" )
+      ( "user,U",
+           boost::program_options::value<std::string>(&user),
+           "User" )
 
       ( "address,A",
            boost::program_options::value<std::string>(&localAddressString)->default_value("::"),
@@ -131,6 +153,11 @@ int main(int argc, char *argv[])
 
    // ====== Initialize =====================================================
    initialiseLogger(logLevel);
+   const passwd* pw = getUser(user.c_str());
+   if(pw == nullptr) {
+      HPCT_LOG(fatal) << "Cannot find user \"" << user << "\"!";
+      return 1;
+   }
 
    boost::asio::io_service        ioService;
    boost::asio::ip::udp::endpoint localEndpoint(localAddress, localPort);
@@ -143,6 +170,13 @@ int main(int argc, char *argv[])
    if(errorCode !=  boost::system::errc::success) {
       HPCT_LOG(fatal) << "Unable to bind UDP socket to source address "
                       << localEndpoint << ": " << errorCode.message();
+      return 1;
+   }
+
+
+   // ====== Reduce privileges ==============================================
+   if(reducePrivileges(pw) == false) {
+      HPCT_LOG(fatal) << "Failed to reduce privileges!";
       return 1;
    }
 
