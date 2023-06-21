c250c27dd0253fd68a11c4ff6f89e804c97c486d
Thomas Dreibholz <dreibh@simula.no>
2023-06-21 16:23:12 +0200

    A fix.

--- src/database-statement.h
+++ src/database-statement.h
@@ -62,7 +62,7 @@ class Statement : public std::stringstream
    }
 
    inline bool isValid() const {
-      return (!InTuple) && (Rows > 0);
+      return (!InTuple) && ((Rows > 0) || (!str().empty()));
    }
 
    inline size_t getRows() const {
