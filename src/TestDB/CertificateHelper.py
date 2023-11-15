5df191a4694dc908eed512fa67f5d403a486bb6a
Thomas Dreibholz <dreibh@simula.no>
2023-11-15 12:17:05 +0100

    Header updates.

--- src/TestDB/CertificateHelper.py
+++ src/TestDB/CertificateHelper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #
 # Create CA Hierarchy and Certificates
-# Copyright (C) 2022-2023 by Thomas Dreibholz
+# Copyright (C) 2022-2024 by Thomas Dreibholz
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
