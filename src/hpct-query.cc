b2aafa6b5299a9df9cc46bb30cbcbadad65d27ce
Thomas Dreibholz <dreibh@simula.no>
2024-01-26 12:37:40 +0100

    Disabled experimental code in this branch.

--- src/hpct-query.cc
+++ src/hpct-query.cc
@@ -626,6 +626,7 @@ int main(int argc, char** argv)
          }
       }
 
+#if 0
       // ====== Jitter ======================================================
       else if(queryType == "jitter") {
          if(backend & DatabaseBackendType::SQL_Generic) {
@@ -828,6 +829,7 @@ int main(int argc, char** argv)
             abort();
          }
       }
+#endif
 
       // ====== Invalid query ===============================================
       else {
