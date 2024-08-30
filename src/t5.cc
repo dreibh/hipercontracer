4e3dfa621b3ba589c07246ab3c0f20604ca54fa3
Thomas Dreibholz <dreibh@simula.no>
2024-08-30 17:06:03 +0200

    More testing.

--- src/t5.cc
+++ src/t5.cc
@@ -9,12 +9,12 @@ int main(int argc, char** argv)
 {
    const unsigned long long testTimeStamp = 0x17972cfc4c932d87ULL;
    const ReaderTimePoint    testTimePoint = nanosecondsToTimePoint<ReaderTimePoint>(testTimeStamp);
-   std::cout << "now :  ts=" << testTimeStamp
+   std::cout << "test:  ts=" << testTimeStamp
              << "\ttp=" << timePointToString<ReaderTimePoint>(testTimePoint, 9) << "\n";
 
    const ReaderTimePoint    nowTimePoint  = ReaderClock::now();
    const unsigned long long nowTimeStamp  = timePointToNanoseconds<ReaderTimePoint>(nowTimePoint);
-   std::cout << "now :  ts=" << nowTimeStamp
+   std::cout << "now:   ts=" << nowTimeStamp
              << "\ttp=" << timePointToString<ReaderTimePoint>(nowTimePoint, 9) << "\n";
 
    if(testTimeStamp > nowTimeStamp) {
