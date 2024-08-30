1d3ac93fef80c71120cffe98f19064fe6e797afc
Thomas Dreibholz <dreibh@simula.no>
2024-08-30 16:52:43 +0200

    Some testing.

--- src/t5.cc
+++ src/t5.cc
@@ -8,8 +8,14 @@ typedef ReaderClock::duration                ReaderTimeDuration;
 int main(int argc, char** argv)
 {
    const unsigned long long ts = 0x17972cfc4c932d87ULL;
+
+   const ReaderTimePoint now       = ReaderClock::now();
    const ReaderTimePoint timeStamp = nanosecondsToTimePoint<ReaderTimePoint>(ts);
 
+   std::cout << "n=" << timePointToString<ReaderTimePoint>(now, 9)       << "\n";
    std::cout << "t=" << timePointToString<ReaderTimePoint>(timeStamp, 9) << "\n";
+   if(now < timeStamp) {
+      std::cerr << "Jævla faen!\n";
+   }
    return 0;
 }
