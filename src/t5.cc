baa594b7a77cfc1fa4c9c6eff50a1dd1dbe8a154
Thomas Dreibholz <dreibh@simula.no>
2024-08-30 16:44:39 +0200

    Another test.

--- src/t5.cc
+++ src/t5.cc
@@ -1,15 +1,15 @@
 #include "tools.h"
 #include <iostream>
 
-typedef std::chrono::steady_clock            ResultClock;
-typedef std::chrono::time_point<ResultClock> ResultTimePoint;
-typedef ResultClock::duration                ResultDuration;
+typedef std::chrono::high_resolution_clock   ReaderClock;
+typedef std::chrono::time_point<ReaderClock> ReaderTimePoint;
+typedef ReaderClock::duration                ReaderTimeDuration;
 
 int main(int argc, char** argv)
 {
    const unsigned long long ts = 0x17972cfc4c932d87ULL;
-   const ResultTimePoint timeStamp = nanosecondsToTimePoint<ResultTimePoint>(ts);
+   const ReaderTimePoint timeStamp = nanosecondsToTimePoint<ReaderTimePoint>(ts);
 
-   std::cout << "t=" << timePointToString<ResultTimePoint>(timeStamp, 9) << "\n";
+   std::cout << "t=" << timePointToString<ReaderTimePoint>(timeStamp, 9) << "\n";
    return 0;
 }
