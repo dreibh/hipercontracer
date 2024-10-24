1b01c19661f1fbf21e3ca54b83ca59690ed6d191
Thomas Dreibholz <dreibh@simula.no>
2024-10-24 09:47:58 +0200

    Improved library.

--- src/assure.cc
+++ src/assure.cc
@@ -1,42 +1,60 @@
-/*
- * ==========================================================================
- *         _   _      _   ____            __ __  __      _
- *        | \ | | ___| |_|  _ \ ___ _ __ / _|  \/  | ___| |_ ___ _ __
- *        |  \| |/ _ \ __| |_) / _ \ '__| |_| |\/| |/ _ \ __/ _ \ '__|
- *        | |\  |  __/ |_|  __/  __/ |  |  _| |  | |  __/ ||  __/ |
- *        |_| \_|\___|\__|_|   \___|_|  |_| |_|  |_|\___|\__\___|_|
- *
- *                  NetPerfMeter -- Network Performance Meter
- *                 Copyright (C) 2009-2024 by Thomas Dreibholz
- * ==========================================================================
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contact:  dreibh@simula.no
- * Homepage: https://www.nntb.no/~dreibh/netperfmeter/
- */
+// ==========================================================================
+//     _   _ _ ____            ____          _____
+//    | | | (_)  _ \ ___ _ __ / ___|___  _ _|_   _| __ __ _  ___ ___ _ __
+//    | |_| | | |_) / _ \ '__| |   / _ \| '_ \| || '__/ _` |/ __/ _ \ '__|
+//    |  _  | |  __/  __/ |  | |__| (_) | | | | || | | (_| | (_|  __/ |
+//    |_| |_|_|_|   \___|_|   \____\___/|_| |_|_||_|  \__,_|\___\___|_|
+//
+//       ---  High-Performance Connectivity Tracer (HiPerConTracer)  ---
+//                 https://www.nntb.no/~dreibh/hipercontracer/
+// ==========================================================================
+//
+// High-Performance Connectivity Tracer (HiPerConTracer)
+// Copyright (C) 2015-2025 by Thomas Dreibholz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Contact: dreibh@simula.no
 
 #include "assure.h"
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstring>
+#include <iostream>
 
 
 // ###### Print error message and abort #####################################
-void __assure_fail(const char* expression, const char* function)
+void __assure_fail(const char*  expression,
+                   const char*  file,
+                   unsigned int line,
+                   const char*  function)
 {
-   fprintf(stderr, "assure(%s) failed in %s!\n",
-           expression, function);
+   std::cerr << "assure(" << expression << ") failed in "
+             << function << " (" << file << ":" << line << ")!"
+             << std::endl;
+   abort();
+}
+
+
+// ###### Print error message and abort with error from errno ###############
+void __assure_fail_perror(const char*  expression,
+                          const char*  file,
+                          unsigned int line,
+                          const char*  function)
+{
+   std::cerr << "assure(" << expression << ") failed in "
+             << function << " (" << file << ":" << line << "): "
+             << strerror(errno)
+             << std::endl;
    abort();
 }
--- src/assure.h
+++ src/assure.h
@@ -1,31 +1,31 @@
-/*
- * ==========================================================================
- *         _   _      _   ____            __ __  __      _
- *        | \ | | ___| |_|  _ \ ___ _ __ / _|  \/  | ___| |_ ___ _ __
- *        |  \| |/ _ \ __| |_) / _ \ '__| |_| |\/| |/ _ \ __/ _ \ '__|
- *        | |\  |  __/ |_|  __/  __/ |  |  _| |  | |  __/ ||  __/ |
- *        |_| \_|\___|\__|_|   \___|_|  |_| |_|  |_|\___|\__\___|_|
- *
- *                  NetPerfMeter -- Network Performance Meter
- *                 Copyright (C) 2009-2024 by Thomas Dreibholz
- * ==========================================================================
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contact:  dreibh@simula.no
- * Homepage: https://www.nntb.no/~dreibh/netperfmeter/
- */
+// ==========================================================================
+//     _   _ _ ____            ____          _____
+//    | | | (_)  _ \ ___ _ __ / ___|___  _ _|_   _| __ __ _  ___ ___ _ __
+//    | |_| | | |_) / _ \ '__| |   / _ \| '_ \| || '__/ _` |/ __/ _ \ '__|
+//    |  _  | |  __/  __/ |  | |__| (_) | | | | || | | (_| | (_|  __/ |
+//    |_| |_|_|_|   \___|_|   \____\___/|_| |_|_||_|  \__,_|\___\___|_|
+//
+//       ---  High-Performance Connectivity Tracer (HiPerConTracer)  ---
+//                 https://www.nntb.no/~dreibh/hipercontracer/
+// ==========================================================================
+//
+// High-Performance Connectivity Tracer (HiPerConTracer)
+// Copyright (C) 2015-2025 by Thomas Dreibholz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Contact: dreibh@simula.no
 
 #ifndef ASSURE_H
 #define ASSURE_H
@@ -33,9 +33,22 @@
 // Assertion like assert(), but which will be checked in all build types,
 // i.e. not only for Debug builds but also in Release builds
 #define assure(expression) \
-   if(__builtin_expect(!(expression), 0)) __assure_fail(#expression, __func__);
+   if(__builtin_expect(!(expression), 0)) { \
+      __assure_fail(#expression, __FILE__, __LINE__, __func__); \
+   }
 
-void __assure_fail(const char* expression,
-                   const char* function);
+#define assure_perror(expression) \
+   if(__builtin_expect(!(expression), 0)) { \
+      __assure_fail_perror(#expression, __FILE__, __LINE__, __func__); \
+   }
+
+void __assure_fail(const char*  expression,
+                   const char*  file,
+                   unsigned int line,
+                   const char*  function) __attribute__ ((__noreturn__));
+void __assure_fail_perror(const char*  expression,
+                          const char*  file,
+                          unsigned int line,
+                          const char*  function) __attribute__ ((__noreturn__));
 
 #endif
