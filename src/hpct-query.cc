fc7c784bf84a42fa3fcd67b16f9376be754bd589
Thomas Dreibholz <dreibh@simula.no>
2024-12-10 11:06:20 +0100

    Further version 1 MongoDB export fixes.

--- src/hpct-query.cc
+++ src/hpct-query.cc
@@ -644,7 +644,7 @@ int main(int argc, char** argv)
                      const long long                delayAppSend    = -1;
                      const long long                delayQueuing    = -1;
                      const long long                delayAppReceive = -1;
-                     const long long                rttApplication  = databaseClient->getBigInt("rtt");
+                     const long long                rttApplication  = 1000 * databaseClient->getBigInt("rtt");
                      const long long                rttSoftware     = 0;
                      const long long                rttHardware     = 0;
                      OUTPUT_PING_V2;
@@ -833,10 +833,10 @@ int main(int argc, char** argv)
                         const boost::asio::ip::address hopIP           = statement.decodeAddress(databaseClient->getString("hop"));
                         const unsigned int             status          = databaseClient->getInteger("status");
                         const unsigned int             timeSource      = 0x00000000;
-                        const long long                delayAppSend    = 0;
-                        const long long                delayQueuing    = 0;
-                        const long long                delayAppReceive = 0;
-                        const long long                rttApplication  = databaseClient->getBigInt("rtt");
+                        const long long                delayAppSend    = -1;
+                        const long long                delayQueuing    = -1;
+                        const long long                delayAppReceive = -1;
+                        const long long                rttApplication  = 1000 * databaseClient->getBigInt("rtt");
                         const long long                rttSoftware     = -1;
                         const long long                rttHardware     = -1;
                         OUTPUT_TRACEROUTE_HOP_V2;
