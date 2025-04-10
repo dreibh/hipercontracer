5c40b515aef6cfe5f24a225d58045af48b74358b
Thomas Dreibholz <dreibh@simula.no>
2025-04-10 09:39:06 +0200

    Bugfix: Fixed handling of long options, to prevent ambiguity with short options.

--- src/udp-echo-server.cc
+++ src/udp-echo-server.cc
@@ -189,8 +189,7 @@ int main(int argc, char *argv[])
    try {
       boost::program_options::store(boost::program_options::command_line_parser(argc, argv).
                                        style(
-                                          boost::program_options::command_line_style::style_t::default_style|
-                                          boost::program_options::command_line_style::style_t::allow_long_disguise
+                                          boost::program_options::command_line_style::style_t::unix_style
                                        ).
                                        options(commandLineOptions).
                                        run(), vm);
