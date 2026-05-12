812182bd32098e4d1e9d3fadec91796c7200879d
Thomas Dreibholz <dreibh@simula.no>
2026-05-12 12:47:12 +0200

    Minor improvements.

--- src/hpct-trigger.cc
+++ src/hpct-trigger.cc
@@ -664,16 +664,16 @@ int main(int argc, char** argv)
          for(std::set<uint8_t>::iterator trafficClassIterator = sourceIterator->second.begin();
              trafficClassIterator != sourceIterator->second.end(); trafficClassIterator++) {
             const uint8_t trafficClass = *trafficClassIterator;
-            std::cout << destinationAddress << " " << (unsigned int)trafficClass << std::endl;
+            // std::cout << destinationAddress << " " << (unsigned int)trafficClass << std::endl;
             destinationsForSource.insert(DestinationInfo(destinationAddress, trafficClass));
          }
       }
 
 
-      for(std::set<DestinationInfo>::iterator iterator = destinationsForSource.begin();
-          iterator != destinationsForSource.end(); iterator++) {
-         std::cout << " -> " << *iterator << std::endl;
-      }
+      // for(std::set<DestinationInfo>::iterator iterator = destinationsForSource.begin();
+      //     iterator != destinationsForSource.end(); iterator++) {
+      //    std::cout << " -> " << *iterator << std::endl;
+      // }
 
       for(const std::string& ioModule : ioModules) {
 #if 0
